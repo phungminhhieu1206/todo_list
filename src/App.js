bd9c5c5b6f12aa7cfb9c3fbd3fb1f7618c69787a
Phung Minh Hieu <phungminhhieu1206@gmail.com>
2021-07-27 00:21:44 +0700

    create TodoList component and create form textfile add todo ...

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Textfield from '@atlaskit/textfield';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3>Danh sách cần làm</h3>
+      <Textfield name="add-todo" placeholder="Thêm việc cần làm ..."></Textfield>
+    </>
   );
 }
 
