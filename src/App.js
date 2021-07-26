581f44a7827cdf27fd3ef0456244994461d61055
Phung Minh Hieu <phungminhhieu1206@gmail.com>
2021-07-27 00:28:08 +0700

    add button after textfield by @atlaskit/button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Textfield from '@atlaskit/textfield';
+import Button from '@atlaskit/button';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <>
       <h3>Danh sách cần làm</h3>
-      <Textfield name="add-todo" placeholder="Thêm việc cần làm ..."></Textfield>
+      <Textfield name="add-todo" placeholder="Thêm việc cần làm ..." elemAfterInput={
+        <Button isDisabled={true} appearance='primary'>
+          Thêm
+        </Button>}
+        css={{ padding: "2px 4px 2px" }}
+      ></Textfield>
+
+      <TodoList/>
     </>
   );
 }
